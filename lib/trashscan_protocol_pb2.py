b2cbf21017ee7833d51b5b48afbcfa067d94b354
Svene <hello.svene@gmail.com>
2024-07-15 22:39:59 +0800

    chore: naming convention

--- lib/trashscan_protocol_pb2.py
+++ lib/trashscan_protocol_pb2.py
@@ -9,11 +9,11 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18_protocl.proto\"T\n\nBIN_STATUS\x12\x10\n\x08SENSOR_1\x18\x01 \x01(\x02\x12\x10\n\x08SENSOR_2\x18\x02 \x01(\x02\x12\x10\n\x08SENSOR_3\x18\x03 \x01(\x02\x12\x10\n\x08SENSOR_4\x18\x04 \x01(\x02\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18trashscan_protocol.proto\"T\n\nBIN_STATUS\x12\x10\n\x08SENSOR_1\x18\x01 \x01(\x02\x12\x10\n\x08SENSOR_2\x18\x02 \x01(\x02\x12\x10\n\x08SENSOR_3\x18\x03 \x01(\x02\x12\x10\n\x08SENSOR_4\x18\x04 \x01(\x02\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'protocol', _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'trashscan_protocol_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_BIN_STATUS']._serialized_start=28
