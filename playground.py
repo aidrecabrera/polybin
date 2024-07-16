fb9484b7c5acf550f4ab986059dd71d2b2ea1bfe
Svene <hello.svene@gmail.com>
2024-07-17 02:28:17 +0800

    fix: data retrieval and print statements

--- playground.py
+++ playground.py
@@ -4,5 +4,8 @@ from lib.data import Data
 data = Data("/dev/ttyUSB0")
 while True:
     data.retrieve_data()
-    for i in range(1, 5):
-        print(f"sensor_{i}: {getattr(data, f'sensor_{i}')}")
+    print(data.sensor_1)
+    print(data.sensor_2)
+    print(data.sensor_3)
+    print(data.sensor_4)
+    
