45eeb0c639038d4450147181f2af247970969cab
Svene <hello.svene@gmail.com>
2024-07-17 02:25:26 +0800

    refactor sensor data retrieval and update

--- lib/data.py
+++ lib/data.py
@@ -1,3 +1,4 @@
+from re import T
 import serial
 import serial.tools.list_ports
 from proto import trashscan_protocol_pb2 as protocol
@@ -5,10 +6,12 @@ from proto import trashscan_protocol_pb2 as protocol
 class Data:
     def __init__(self, serial_port):
         self.serial_port = serial_port
-        self.sensors = {f"SENSOR_{i}": 40 for i in range(1, 5)}
+        self.sensor_1 = 40
+        self.sensor_2 = 40
+        self.sensor_3 = 40
+        self.sensor_4 = 40
 
-    def update(self):
-        print(self.sensors)
+    def retrieve_data(self):
         """update sensor data from serial connection"""
         if not self.check_transmission():
             return False
@@ -18,9 +21,9 @@ class Data:
                 encoded_message = ser.read(20)
                 bin_status = protocol.BIN_STATUS()
                 bin_status.ParseFromString(encoded_message)
-                
                 for i in range(1, 5):
-                    self.sensors[f"SENSOR_{i}"] = getattr(bin_status, f"SENSOR_{i}")
+                    setattr(self, f"sensor_{i}", getattr(bin_status, f"sensor_{i}"))
+                    print(f"sensor_{i}: {getattr(self, f'sensor_{i}')}")
             return True
         except serial.SerialException as e:
             print(f"serial error: {e}")
@@ -29,3 +32,10 @@ class Data:
     def check_transmission(self):
         """check if the specified serial port is available"""
         return self.serial_port in [port.device for port in serial.tools.list_ports.comports()]
+
+if __name__ == "__main__":
+    data = Data("/dev/ttyUSB0")
+    while True:
+        data.retrieve_data()
+        for i in range(1, 5):
+            print(f"sensor_{i}: {getattr(data, f'sensor_{i}')}")
