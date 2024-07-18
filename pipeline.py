371d537f7be26a242f2a88f780d5710163dce3f0
Svene <hello.svene@gmail.com>
2024-07-18 23:30:14 +0800

    update pipeline creds

--- pipeline.py
+++ pipeline.py
@@ -2,9 +2,9 @@ from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
 pipeline = InferencePipeline.init(
-    model_id="model_id/version",
-    video_reference=0,
-    on_prediction=render_boxes, # Function to run after each prediction
+    model_id="garbage-segregator-ndyo4/5", 
+    video_reference=0, 
+    on_prediction=render_boxes, 
 )
 pipeline.start()
 pipeline.join()
