e885021dbb99a4e014ace7fcf4fa81cd55fc6554
Svene <hello.svene@gmail.com>
2024-08-01 08:02:30 +0800

    refactor: update Dispose class to gradually set servo angles

--- lib/dispose.py
+++ lib/dispose.py
@@ -16,39 +16,40 @@ class Dispose:
         self.last_action_time = time.time()
         self.servo_lock = threading.Lock()
 
-    def set_servo_angle(self, servo_pin, angle):
+    def gradual_set_servo_angle(self, servo_pin, start_angle, end_angle, step=1, delay=0.05):
         with self.servo_lock:
             pwm = GPIO.PWM(servo_pin, 50)
             pwm.start(0)
-            duty = angle / 18 + 2
-            GPIO.output(servo_pin, GPIO.HIGH)
-            pwm.ChangeDutyCycle(duty)
-            time.sleep(1)
+            for angle in range(start_angle, end_angle + step, step):
+                duty = angle / 18 + 2
+                GPIO.output(servo_pin, GPIO.HIGH)
+                pwm.ChangeDutyCycle(duty)
+                time.sleep(delay)
             GPIO.output(servo_pin, GPIO.LOW)
             pwm.ChangeDutyCycle(0)
             pwm.stop()
 
-    def dispose(self, servo1_angle, servo2_angle):
-        self.set_servo_angle(self.SERVO_PIN_1, servo1_angle)
-        self.set_servo_angle(self.SERVO_PIN_2, servo2_angle)
+    def dispose(self, servo1_start_angle, servo1_end_angle, servo2_start_angle, servo2_end_angle):
+        self.gradual_set_servo_angle(self.SERVO_PIN_1, servo1_start_angle, servo1_end_angle)
+        self.gradual_set_servo_angle(self.SERVO_PIN_2, servo2_start_angle, servo2_end_angle)
         time.sleep(1)
-        self.set_servo_angle(self.SERVO_PIN_2, 90)
+        self.gradual_set_servo_angle(self.SERVO_PIN_2, servo2_end_angle, 90)
 
     def dispose_biodegradable(self):
         print("Performing action: Disposing Biodegradable")
-        self.dispose(0, 0)
+        self.dispose(0, 0, 0, 0)
 
     def dispose_non_biodegradable(self):
         print("Performing action: Disposing Non-Biodegradable")
-        self.dispose(90, 0)
+        self.dispose(0, 90, 0, 0)
 
     def dispose_recyclable(self):
         print("Performing action: Disposing Recyclable")
-        self.dispose(0, 180)
+        self.dispose(0, 0, 0, 180)
 
     def dispose_hazardous(self):
         print("Performing action: Disposing Dangerous/Hazardous Waste")
-        self.dispose(90, 180)
+        self.dispose(0, 90, 0, 180)
 
     def can_perform_action(self):
         current_time = time.time()
